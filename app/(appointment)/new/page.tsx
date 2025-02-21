8b731c7ef57a268b9e6c4a00366426ea5181861c
EngJao89 <eng_joao@outlook.com>
2025-02-21 17:28:12 -0300

    feat(mgb13-date-time-picker): create a date time picker for schedule date

--- app/(appointment)/new/page.tsx
+++ app/(appointment)/new/page.tsx
@@ -1,26 +1,49 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { router } from "expo-router";
 import Ionicons from '@expo/vector-icons/Ionicons';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 import { Colors } from "@/constants/Colors";
 
 export default function Appointment () {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const minimumDate = new Date(2020, 0, 1);
+
   return (
     <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <Ionicons name="arrow-back-outline" size={32} style={styles.icon}/>
+        </TouchableOpacity>
+
+        <Text style={styles.headerTitle}>Agendamento</Text>
+
+        <Image source={{ uri: 'https://github.com/EngJao89.png' }} style={styles.profile}/>
+      </View>
+
       <View style={styles.container}>
-        <View style={styles.header}>
-          <View>
-            <Ionicons name="arrow-back-outline" size={32} color={Colors.zinc_200}/>
-          </View>
 
-          <View>
-            <Text style={styles.headerTitle}>Agendamento</Text>
-          </View>
-          
+        <View style={styles.dateContainer}>
+          <Text style={styles.title}>Escolha a data</Text>
 
-          <View>
-            <Image source={{ uri: 'https://github.com/EngJao89.png' }} style={styles.profile}/>
-          </View>
+          <DateTimePicker 
+            mode="date"
+            is24Hour={true}
+            display="spinner" 
+            value={selectedDate}
+            onChange={(event, date) => {
+              if (date) {
+                setSelectedDate(date);
+              }
+            }}
+            minimumDate={minimumDate}
+            textColor="#f4ede8"
+          />
         </View>
+        
+
+        <View></View>
       </View>
     </SafeAreaView>
   )
@@ -32,26 +55,41 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.zinc_900,
   },
   container: {
+    backgroundColor: Colors.zinc_800,
+    paddingTop: 24,
     paddingLeft: 16,
     paddingRight: 16,
     marginBottom: 180,
   },
   header: {
     width: '100%',
+    marginTop: 24,
     marginBottom: 36,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'baseline',
+    alignItems: 'center',
   },
   headerTitle: {
     color: Colors.zinc_100,
     fontSize: 28,
     fontWeight: 'bold',
   },
+  dateContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    color: Colors.zinc_400,
+  },
   profile: {
     width: 56,
     height: 56,
     borderRadius: 32,
   },
+  title: {
+    color: Colors.zinc_100,
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
 });
-
