59a9acd0bb37105f01bcc6c09d1ab76a0d58c98d
EngJao89 <eng_joao@outlook.com>
2025-04-12 19:18:46 -0300

    feat(mgb55-barber-scheduling): create service for barbers work hours

--- app/(appointment)/detailsbarber/[id].tsx
+++ app/(appointment)/detailsbarber/[id].tsx
@@ -1,38 +1,46 @@
-import { Alert, Image, Platform, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { Colors } from "@/constants/Colors";
-import { Loading } from "@/components/Loading";
-import { NotFound } from "@/components/NotFound";
-import api from "@/lib/axios";
+import { 
+  Alert, 
+  Image, 
+  Platform, 
+  SafeAreaView, 
+  StyleSheet, 
+  Text, 
+  TouchableOpacity, 
+  View 
+} from "react-native";
 import { useEffect, useState } from "react";
 import { router, useLocalSearchParams } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
-interface SchedulingData {
+import api from "@/lib/axios";
+import { Colors } from "@/constants/Colors";
+import { Loading } from "@/components/Loading";
+import { NotFound } from "@/components/NotFound";
+
+interface BarberSchedulingData {
   id: string;
-  userId: string;
   barberId: string;
   dayAt: string;
-  hourAt: string;
-  serviceType: string;
-  status: string;
+  startTime: string;
+  endTime: string
 }
 
 export default function  AppointmentDetailsUser(){
   const [loading, setLoading] = useState(true);
-  const [scheduling, setScheduling] = useState<SchedulingData | null>(null);
+  const [barberScheduling, setBarberScheduling] = useState<BarberSchedulingData | null>(null);
   const { id } = useLocalSearchParams();
 
   useEffect(() => {
     const fetchSchedulingDetails = async () => {
       try {
-        const schedulingId = Array.isArray(id) ? id[0] : id;
+        const barberSchedulingId = Array.isArray(id) ? id[0] : id;
         
-        if (!schedulingId) {
+        if (!barberSchedulingId) {
           throw new Error("ID do agendamento não fornecido");
         }
 
-        const response = await api.get(`scheduling/${schedulingId}`);
-        setScheduling(response.data);
+        const response = await api.get(`barber-availability/${barberSchedulingId}`);
+        setBarberScheduling(response.data);
       } catch (error) {
         Alert.alert("Erro ao carregar os dados do agendamento");
         console.error(error);
@@ -50,13 +58,13 @@ export default function  AppointmentDetailsUser(){
     );
   }
 
-  if (!scheduling) {
+  if (!barberScheduling) {
     return (
       <NotFound />
     );
   }
 
-  const formattedDate = new Date(scheduling.dayAt).toLocaleDateString('pt-BR', {
+  const formattedDate = new Date(barberScheduling.dayAt).toLocaleDateString('pt-BR', {
     day: '2-digit',
     month: '2-digit',
     year: 'numeric',
@@ -69,9 +77,34 @@ export default function  AppointmentDetailsUser(){
           <Ionicons name="arrow-back-outline" size={32} style={styles.icon} />
         </TouchableOpacity>
 
-        <Text style={styles.headerTitle}>{scheduling.serviceType}</Text>
+        <Text style={styles.headerTitle}>{formattedDate}</Text>
 
         <Image source={{ uri: 'https://github.com/EngJao89.png' }} style={styles.profile} />
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.detailItem}>
+          <Ionicons name="calendar-outline" size={24} color={Colors.orange_700} />
+          <Text style={styles.detailText}>Data: {formattedDate}</Text>
+        </View>
+
+        <View style={styles.detailItem}>
+          <Ionicons name="time-outline" size={24} color={Colors.orange_700} />
+          <Text style={styles.detailText}>Inicia: {barberScheduling.startTime}</Text>
+        </View>
+
+        <View style={styles.detailItem}>
+          <Ionicons name="time-outline" size={24} color={Colors.orange_700} />
+          <Text style={styles.detailText}>Inicia: {barberScheduling.endTime}</Text>
+        </View>
+
+        <TouchableOpacity activeOpacity={0.7} style={styles.alterButton}>
+          <Text>Alterar Horários</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity activeOpacity={0.7} style={styles.cancelButton}>
+          <Text>Cancelar Horários</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
@@ -103,5 +136,38 @@ const styles = StyleSheet.create({
     width: 56,
     height: 56,
     borderRadius: 32,
+  },
+  container: {
+    backgroundColor: Colors.zinc_800,
+    padding: 24,
+    marginHorizontal: 16,
+    borderRadius: 8,
+    gap: 16,
+  },
+  detailItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+  },
+  detailText: {
+    color: Colors.zinc_100,
+    fontSize: 18,
+  },
+  cancelButton: {
+    backgroundColor: Colors.red_600,
+    marginTop: 4,
+    marginBottom: 24,
+    paddingTop: 14,
+    paddingBottom: 14,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  alterButton: {
+    backgroundColor: Colors.orange_600,
+    marginTop: 24,
+    paddingTop: 14,
+    paddingBottom: 14,
+    borderRadius: 8,
+    alignItems: "center",
   },
 });
